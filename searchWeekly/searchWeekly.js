257e8c24bce245eb06c1d9fa62512a05f3d4a4a9
boyon99 <bo_yon_99@naver.com>
2023-02-11 23:46:36 +0900

    feat: Refactor code

--- searchWeekly/searchWeekly.js
+++ searchWeekly/searchWeekly.js
@@ -1,10 +1,10 @@
-let key = "?key=44d03c21429c1454bf58532d157bbc75" // key prameter
+let key = process.env.key // key prameter
 let targetDt = "&targetDt=20220101" // targetDt
 const url = "http://www.kobis.or.kr/kobisopenapi/webservice/rest/boxoffice/searchWeeklyBoxOfficeList.json"
   + key
   + targetDt
 
-let container = document.getElementById('a') // html에서 나타날 요소 선택
+let container = document.getElementById('container') // 소를 담을 컨데이너 생성
 
 
 fetch(url)
@@ -13,22 +13,19 @@ fetch(url)
     for (let i = 0; i < 20; i++) {
       // div 생성 
       let div = document.createElement('div'); 
-
-      // id 부여
       div.id = 'movies'
 
       // api를 리스트에 담기
       search_movie_list = item.boxOfficeResult.weeklyBoxOfficeList[i]
-      console.log(search_movie_list);
 
       // 요소 생성
       let movieNm = document.createElement('p')
       let openDt = document.createElement('p')
       let rank = document.createElement('p')
 
-      movieNm.innerText = search_movie_list.movieNm
-      openDt.innerText = search_movie_list.openDt
-      rank.innerText = search_movie_list.rank
+      movieNm.innerText = search_movie_list.movieNm // 영화이름
+      openDt.innerText = search_movie_list.openDt // 개봉날짜
+      rank.innerText = search_movie_list.rank // 순위
 
 
       // html에 요소 담기
@@ -36,7 +33,6 @@ fetch(url)
       div.appendChild(openDt);
       div.appendChild(rank);
       container.appendChild(div);
-
     }
 
   });
